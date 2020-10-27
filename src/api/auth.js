19918f3264bafbf7f45a8a2ab7caa43a81f09fd6
xiaobaihena <2507246051@qq.com>
2020-10-27 16:39:22 +0800

    get captcha code

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,24 +1,32 @@
-import { request } from '@/utils/request'
+import { request } from '@/utils/request';
 
 export function login(data) {
   return request('weapp/authorizations', {
     method: 'post',
-    data: data
-  })
+    data: data,
+  });
 }
 export function refresh(token) {
   return request('authorizations/current', {
     method: 'put',
     header: {
-      'Authorization': 'Bearer' + token
-    }
-  })
+      Authorization: 'Bearer' + token,
+    },
+  });
 }
 export function logout(token) {
   return request('authorizations/current', {
     method: 'delete',
     header: {
-      'Authorization': 'Bearer' + token
-    }
-  })
+      Authorization: 'Bearer' + token,
+    },
+  });
+}
+export function getCaptcha(phone) {
+  return request('captchas', {
+    method: 'post',
+    data: {
+      phone: phone,
+    },
+  });
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,72 +1,69 @@
-import wepy from '@wepy/core'
-import store from '@/store'
+import wepy from '@wepy/core';
+import store from '@/store';
 
 // 服务器接口地址
-const host = 'https://larabbs.learnku.fit/api/v1/'
+const host = 'https://larabbs.learnku.fit/api/v1/';
 
 // 普通请求
 const request = async (url, options = {}, showLoading = true) => {
   // 显示加载中
   if (showLoading) {
-    wx.showLoading({title: '加载中'})
+    wx.showLoading({ title: '加载中' });
   }
   // 拼接请求地址
-  options.url = host + url
+  options.url = host + url;
 
-  let response = await wepy.wx.request(options)
+  let response = await wepy.wx.request(options);
 
   if (showLoading) {
     // 隐藏加载中
-    wx.hideLoading()
+    wx.hideLoading();
   }
 
   if (response.statusCode >= 200 && response.statusCode < 300) {
-    return response
+    return response;
   }
 
   if (response.statusCode === 429) {
     wx.showModal({
       title: '提示',
-      content: '请求太频繁，请稍后再试'
-    })
+      content: '请求太频繁，请稍后再试',
+    });
   }
 
   if (response.statusCode === 500) {
     wx.showModal({
       title: '提示',
-      content: '服务器错误，请联系管理员或重试'
-    })
+      content: '服务器错误，请联系管理员或重试',
+    });
   }
 
-  const error = new Error(response.data.message)
-  error.response = response
-  return Promise.reject(error)
-}
+  const error = new Error(response.data.message);
+  error.response = response;
+  return Promise.reject(error);
+};
 const checkToken = async () => {
   // 从缓存中取出 Token
-  const accessToken = store.getters.accessToken
-  const expiredAt = store.getters.accessTokenExpiredAt
+  const accessToken = store.getters.accessToken;
+  const expiredAt = store.getters.accessTokenExpiredAt;
 
   // 如果 token 过期了，则调用刷新方法
   if (accessToken && new Date().getTime() > expiredAt) {
     try {
-      return store.dispatch('refresh')
+      return store.dispatch('refresh');
     } catch (err) {
-      return store.dispatch('login')
+      return store.dispatch('login');
     }
   }
-}
+};
 
 //普通请求
 const authRequest = async (url, options = {}, showLoading = true) => {
-  await checkToken()
+  await checkToken();
   options.header = {
-    Authorization: 'Bearer' + store.getters.accessToken
-  }
-  return await request(url, options, showLoading)
-}
+    Authorization: 'Bearer' + store.getters.accessToken,
+  };
+  return await request(url, options, showLoading);
+};
 
-export {
- request,
- authRequest
-}
+export { request, authRequest };
