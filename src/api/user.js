72ae66da4eb99f2f2e646ebe5be5926079336688
xiaobaihena <2507246051@qq.com>
2020-11-03 19:11:00 +0800

    update avatar

--- src/api/user.js
+++ src/api/user.js
@@ -1,10 +1,22 @@
-import { request, authRequest } from '@/utils/request';
+import { request, authRequest, uploadFile } from '@/utils/request';
 export function getCurrentUser(data) {
   return authRequest('user');
 }
+//更新用户信息
 export function updateUser(data) {
   return authRequest('user', {
     method: 'put',
     data: data,
+  });
+}
+//上传头像
+export function updateAvatar(avatar) {
+  return uploadFile('images', {
+    method: 'POST',
+    name: 'image',
+    formData: {
+      type: 'avatar',
+    },
+    filePath: avatar,
   });
 }
